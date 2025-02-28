90afd2873f9c18a26e6983b237ac27f9a0f04916
Stage777999 <ikingib1979@gmail.com>
2025-02-28 21:25:41 +0500

    end

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React, { Children } from "react";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import BookList from "./components/BookList";
+import AddBook from "./components/AddBook";
+import EditBook from "./components/EditBook";
+import './App.css'
+
+const App = () => {
+
+  return (
+    <Router>
+      <div>
+        <h1 style={{ textAlign: "center" }}>№81 group name</h1>
+        <Routes>
+             <Route path="/add" element={<AddBook />} />
+          <Route path="/" element={<BookList />} />
+          <Route path="/edit/:id" element={<EditBook />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
+
